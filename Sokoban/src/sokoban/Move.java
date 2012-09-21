bf5a638838c53c5a21654c490d268302b41e5d84
Arash Rouhani <miffoljud@gmail.com>
2012-09-21 18:23:32 -0400

    Move out Move class from Sokoban class

---
+++ Sokoban/src/sokoban/Move.java
@@ -0,0 +1,40 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sokoban;
+
+import java.awt.Point;
+
+/**
+ *
+ * @author arash
+ */
+public class Move {
+
+    public int dir;
+
+    public Move(int dir) {
+        this.dir = dir;
+    }
+
+    static public Move[] directions() {
+        Move[] dirs = {new Move(0), new Move(1), new Move(2), new Move(3)};
+        return dirs;
+    }
+
+    public Point movePoint(Point p) {
+        return new Point(p.x + dx[dir], p.y + dy[dir]);
+    }
+    public static int dy[] = {1, 0, -1, 0};
+    public static int dx[] = {0, 1, 0, -1};
+
+    private Move reverse() {
+        return new Move((dir + 2) % 4);
+    }
+
+    @Override
+    public String toString() {
+        return "" + dir;
+    }
+}
--- Sokoban/src/sokoban/Sokoban.java
+++ Sokoban/src/sokoban/Sokoban.java
@@ -30,34 +30,6 @@ public class Sokoban {
         return true;
     }
 
-    public static class Move {
-
-        public int dir;
-
-        public Move(int dir) {
-            this.dir = dir;
-        }
-
-        static public Move[] directions() {
-            Move[] dirs = {new Move(0), new Move(1), new Move(2), new Move(3)};
-            return dirs;
-        }
-
-        public Point movePoint(Point p) {
-            return new Point(p.x + dx[dir], p.y + dy[dir]);
-        }
-        public int dy[] = {1, 0, -1, 0};
-        public int dx[] = {0, 1, 0, -1};
-
-        private Move reverse() {
-            return new Move((dir+2)%4);
-        }
-        
-        @Override
-        public String toString(){
-            return ""+ dir;
-        }
-    }
     public Map gameMap;
 
     private Map.SquareType[][] getMap() {
